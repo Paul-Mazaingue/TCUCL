e3a8be7f43c988152d7d0c0736866efdf8228f16
Jessica <jessicaennor@gmail.com>
2025-12-12 10:26:16 +0100

    feat: modify scenario card component

--- Front-TCUCL/frontend/src/app/components/affichage-graphiques/scenario/sub-sections/scenario-card/scenario-card.component.ts
+++ Front-TCUCL/frontend/src/app/components/affichage-graphiques/scenario/sub-sections/scenario-card/scenario-card.component.ts
@@ -23,37 +23,10 @@ export interface Scenario {
 export class ScenarioCardComponent {
   @Input() scenario!: Scenario;
   @Input() isSelected = false;
-  @Output() delete = new EventEmitter<number>();
-  @Output() duplicate = new EventEmitter<Scenario>();
-  @Output() modify = new EventEmitter<Scenario>();
   @Output() select = new EventEmitter<any>();
 
-  isEditing = false;
-  editedScenario: Partial<Scenario> = {};
-
+  
   onSelect() {
     this.select.emit(this.scenario);
-  }
-
-  startEdit() {
-    this.isEditing = true;
-    this.editedScenario = { ...this.scenario };
-  }
-
-  saveEdit() {
-    this.modify.emit(this.editedScenario as Scenario);
-    this.isEditing = false;
-  }
-
-  cancelEdit() {
-    this.isEditing = false;
-  }
-
-  onDuplicate() {
-    this.duplicate.emit(this.scenario);
-  }
-
-  onDelete() {
-    this.delete.emit(this.scenario.id);
   }
 }
